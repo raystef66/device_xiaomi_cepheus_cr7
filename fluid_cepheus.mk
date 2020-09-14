7a95e7568e7b2007f408007503d978dbabfc2b73
Dyrex2004 <dyrex2004@gmail.com>
2020-09-14 19:13:46 +0200

    cepheus: build GApps

--- fluid_cepheus.mk
+++ fluid_cepheus.mk
@@ -21,8 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-# Inherit some common AOSiP stuff.
+# Inherit some common Fluid stuff.
 $(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+$(call inherit-product, vendor/gms/gms_full.mk)
 
 #GApps
 TARGET_BOOT_ANIMATION_RES := 1080
