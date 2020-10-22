5d4c3e73d46a47140d2cb1bcf4bc39a7e2d47482
raystef66 <s.vanbarel@gmail.com>
2020-10-22 14:37:55 +0000

    cepheus: crDroid-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_cepheus.mk
+    $(LOCAL_DIR)/lineage_cepheus.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_cepheus-user \
-    revengeos_cepheus-userdebug \
-    revengeos_cepheus-eng
+    lineage_cepheus-user \
+    lineage_cepheus-userdebug \
+    lineage_cepheus-eng
--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common Fluid stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
-$(call inherit-product, device/xiaomi/cepheus/revengeos_config.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
+$(call inherit-product, device/xiaomi/cepheus/lineage_config.mk)
 
 # Device identifier
-PRODUCT_NAME := revengeos_cepheus
+PRODUCT_NAME := lineage_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
