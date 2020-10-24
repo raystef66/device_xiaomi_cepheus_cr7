c3869b2c035e93f89f11fb8ba3e272ba9e116794
raystef66 <s.vanbarel@gmail.com>
2020-10-24 10:06:27 +0000

    cepheus: add our overlay-lineage

--- device.mk
+++ device.mk
@@ -329,7 +329,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Power
 PRODUCT_PACKAGES += \
