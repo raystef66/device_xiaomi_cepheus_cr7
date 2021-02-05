ed2b6ebe44a94e03475f936e3353cf49b4f7f3c6
raystef66 <s.vanbarel@gmail.com>
2021-02-05 11:07:36 +0000

    Revert "cepheus: uprev display stack"
    This reverts commit 7e59d44073b67eaf73d232ca9c0311af0f92af14.

--- device.mk
+++ device.mk
@@ -151,9 +151,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
-    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service
@@ -161,14 +160,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gralloc.msmnile \
     hwcomposer.msmnile \
-    vendor.display.config@2.0 \
-    vendor.qti.hardware.display.allocator@3.0.vendor \
-    vendor.qti.hardware.display.allocator@4.0.vendor \
-    vendor.qti.hardware.display.mapper@1.0.vendor \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
     memtrack.msmnile
 
 PRODUCT_COPY_FILES += \
